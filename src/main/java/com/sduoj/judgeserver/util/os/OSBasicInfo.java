cabcba61fbd2f86b1158aac22ce718f94f67e002
SongyangJi <1539242799@qq.com>
2021-10-20 10:45:04 +0800

    从 Runtime.getRuntime 获得cpu核心数

--- src/main/java/com/sduoj/judgeserver/util/os/OSBasicInfo.java
+++ src/main/java/com/sduoj/judgeserver/util/os/OSBasicInfo.java
@@ -2,16 +2,13 @@ package com.sduoj.judgeserver.util.os;
 
 
 import org.springframework.stereotype.Service;
-import oshi.SystemInfo;
-import oshi.hardware.CentralProcessor;
+
 
 @Service
 public class OSBasicInfo {
-    private final SystemInfo systemInfo = new SystemInfo();
-    private final CentralProcessor processor = systemInfo.getHardware().getProcessor();
 
     public int getCpuCore() {
-        return processor.getLogicalProcessorCount();
+        return Runtime.getRuntime().availableProcessors();
     }
 
 }
